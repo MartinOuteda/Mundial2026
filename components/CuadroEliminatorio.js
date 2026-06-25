7ba91b21465d0c6d0d927840315a4b6a5e5894f5
MartinOuteda <martinouteda@hotmail.com>
2026-06-25 01:12:50 -0300

    Actualización v25

--- components/CuadroEliminatorio.js
+++ components/CuadroEliminatorio.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import styles from '@/styles/CuadroEliminatorio_v2.module.css';
+import styles from '@/styles/CuadroEliminatorio.module.css';
 
 export default function CuadroEliminatorio() {
   const [cuadro, setCuadro] = useState(null);
