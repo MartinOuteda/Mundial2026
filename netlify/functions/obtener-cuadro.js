0d43fd061821d96be355393dea18807d3bae8175
MartinOuteda <martinouteda@hotmail.com>
2026-06-26 01:44:53 -0300

    v65

--- netlify/functions/obtener-cuadro.js
+++ netlify/functions/obtener-cuadro.js
@@ -1,129 +1,192 @@
 const { Client } = require('pg');
 
+// Mapeo de códigos de país a URLs de banderas
 const BANDERAS = {
-  'ALE': 'https://flagcdn.com/w40/de.png',
-  'BIH': 'https://flagcdn.com/w40/ba.png',
-  'FRA': 'https://flagcdn.com/w40/fr.png',
-  'SUE': 'https://flagcdn.com/w40/se.png',
-  'ALE': 'https://flagcdn.com/w40/de.png',
-  'ECU': 'https://flagcdn.com/w40/ec.png',
-  'ALB': 'https://flagcdn.com/w40/al.png',
-  'ESP': 'https://flagcdn.com/w40/es.png',
-  'ITA': 'https://flagcdn.com/w40/it.png',
-  'HUN': 'https://flagcdn.com/w40/hu.png',
-  'SRB': 'https://flagcdn.com/w40/rs.png',
-  'ROU': 'https://flagcdn.com/w40/ro.png',
-  'GRE': 'https://flagcdn.com/w40/gr.png',
-  'CHE': 'https://flagcdn.com/w40/ch.png',
-  'POL': 'https://flagcdn.com/w40/pl.png',
-  'POR': 'https://flagcdn.com/w40/pt.png',
-  'GHA': 'https://flagcdn.com/w40/gh.png',
-  'BRA': 'https://flagcdn.com/w40/br.png',
-  'JPN': 'https://flagcdn.com/w40/jp.png',
-  'ARG': 'https://flagcdn.com/w40/ar.png',
-  'URU': 'https://flagcdn.com/w40/uy.png',
-  'USA': 'https://flagcdn.com/w40/us.png',
-  'CAN': 'https://flagcdn.com/w40/ca.png',
-  'CRC': 'https://flagcdn.com/w40/cr.png',
   'MEX': 'https://flagcdn.com/w40/mx.png',
-  'EGI': 'https://flagcdn.com/w40/eg.png',
-  'ESC': 'https://flagcdn.com/w40/gb-sct.png',
-  'COL': 'https://flagcdn.com/w40/co.png',
-  'CRO': 'https://flagcdn.com/w40/hr.png',
-  'PB': 'https://flagcdn.com/w40/nl.png',
+  'SUD': 'https://flagcdn.com/w40/za.png',
+  'COR': 'https://flagcdn.com/w40/kr.png',
+  'CZE': 'https://flagcdn.com/w40/cz.png',
+  'CAN': 'https://flagcdn.com/w40/ca.png',
+  'BIH': 'https://flagcdn.com/w40/ba.png',
+  'QAT': 'https://flagcdn.com/w40/qa.png',
+  'SUI': 'https://flagcdn.com/w40/ch.png',
+  'BRA': 'https://flagcdn.com/w40/br.png',
   'MAR': 'https://flagcdn.com/w40/ma.png',
+  'HAI': 'https://flagcdn.com/w40/ht.png',
+  'ESC': 'https://flagcdn.com/w40/gb-sct.png',
+  'USA': 'https://flagcdn.com/w40/us.png',
   'PAR': 'https://flagcdn.com/w40/py.png',
-  'ARL': 'https://flagcdn.com/w40/dz.png',
-  'CDS': 'https://flagcdn.com/w40/kr.png',
   'AUS': 'https://flagcdn.com/w40/au.png',
+  'TUR': 'https://flagcdn.com/w40/tr.png',
+  'ALE': 'https://flagcdn.com/w40/de.png',
+  'CUW': 'https://flagcdn.com/w40/cw.png',
+  'CIV': 'https://flagcdn.com/w40/ci.png',
+  'ECU': 'https://flagcdn.com/w40/ec.png',
+  'HOL': 'https://flagcdn.com/w40/nl.png',
+  'JAP': 'https://flagcdn.com/w40/jp.png',
+  'SUE': 'https://flagcdn.com/w40/se.png',
+  'TUN': 'https://flagcdn.com/w40/tn.png',
+  'BEL': 'https://flagcdn.com/w40/be.png',
+  'EGY': 'https://flagcdn.com/w40/eg.png',
   'IRN': 'https://flagcdn.com/w40/ir.png',
+  'NZL': 'https://flagcdn.com/w40/nz.png',
+  'ESP': 'https://flagcdn.com/w40/es.png',
+  'CPV': 'https://flagcdn.com/w40/cv.png',
+  'ARS': 'https://flagcdn.com/w40/sa.png',
+  'URU': 'https://flagcdn.com/w40/uy.png',
+  'FRA': 'https://flagcdn.com/w40/fr.png',
+  'SEN': 'https://flagcdn.com/w40/sn.png',
+  'IRQ': 'https://flagcdn.com/w40/iq.png',
+  'NOR': 'https://flagcdn.com/w40/no.png',
+  'ARG': 'https://flagcdn.com/w40/ar.png',
+  'ALG': 'https://flagcdn.com/w40/dz.png',
+  'AUT': 'https://flagcdn.com/w40/at.png',
+  'JOR': 'https://flagcdn.com/w40/jo.png',
+  'POR': 'https://flagcdn.com/w40/pt.png',
+  'COG': 'https://flagcdn.com/w40/cg.png',
+  'UZB': 'https://flagcdn.com/w40/uz.png',
+  'COL': 'https://flagcdn.com/w40/co.png',
+  'ING': 'https://flagcdn.com/w40/gb-eng.png',
+  'CRO': 'https://flagcdn.com/w40/hr.png',
+  'GHA': 'https://flagcdn.com/w40/gh.png',
+  'PAN': 'https://flagcdn.com/w40/pa.png'
 };
 
 exports.handler = async (event) => {
-  try {
-    const client = new Client({
-      connectionString: process.env.DATABASE_URL
-    });
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL
+  });
 
+  try {
     await client.connect();
 
-    // Obtener todos los datos necesarios
-    const matchesResult = await client.query('SELECT * FROM cuadro_eliminatorio WHERE fase = \'RO32\' ORDER BY id');
-    const equiposResult = await client.query('SELECT * FROM equipos');
-    const tercerosResult = await client.query(
-      'SELECT id, equipo FROM tabla_posiciones WHERE indice_orden IS NOT NULL ORDER BY indice_orden ASC'
-    );
+    // 1. Obtener 1ros y 2dos de cada grupo
+    const queryPrimerosSegundos = `
+      WITH ranked AS (
+        SELECT 
+          e.id,
+          e.nombre,
+          e.pais_codigo,
+          e.grupo,
+          tp.puntos,
+          tp.goles_a_favor,
+          tp.goles_en_contra,
+          ROW_NUMBER() OVER (
+            PARTITION BY tp.grupo 
+            ORDER BY tp.puntos DESC, (tp.goles_a_favor - tp.goles_en_contra) DESC
+          ) as posicion
+        FROM tabla_posiciones tp
+        JOIN equipos e ON tp.equipo_id = e.id
+      )
+      SELECT * FROM ranked WHERE posicion IN (1, 2)
+      ORDER BY grupo, posicion
+    `;
 
-    const matches = matchesResult.rows;
-    const equipos = equiposResult.rows;
-    const terceros = tercerosResult.rows;
+    // 2. Obtener los 8 mejores terceros
+    const queryTerceros = `
+      WITH ranked AS (
+        SELECT 
+          e.id,
+          e.nombre,
+          e.pais_codigo,
+          e.grupo,
+          tp.puntos,
+          tp.goles_a_favor,
+          tp.goles_en_contra,
+          ROW_NUMBER() OVER (
+            PARTITION BY tp.grupo 
+            ORDER BY tp.puntos DESC, (tp.goles_a_favor - tp.goles_en_contra) DESC
+          ) as posicion
+        FROM tabla_posiciones tp
+        JOIN equipos e ON tp.equipo_id = e.id
+      )
+      SELECT id, nombre, pais_codigo
+      FROM ranked
+      WHERE posicion = 3
+      ORDER BY puntos DESC, (goles_a_favor - goles_en_contra) DESC
+      LIMIT 8
+    `;
 
-    // Mapeo de IDs partidos con terceros y su índice de tercero
-    const partidosConTerceros = {
-      17: 0, // T1
-      18: 1, // T2
-      23: 2, // T3
-      24: 3, // T4
-      27: 4, // T5
-      28: 5, // T6
-      31: 6, // T7
-      32: 7  // T8
-    };
+    // 3. Obtener matches existentes del cuadro
+    const queryMatches = `
+      SELECT * FROM cuadro_eliminatorio
+      ORDER BY id
+    `;
 
-    const buildMatch = (matchId, eq1Key, eq2Key) => {
-      let eq1 = equipos.find(e => e.codigo === eq1Key);
-      let eq2 = equipos.find(e => e.codigo === eq2Key);
+    const [resPrimerosSegundos, resTerceros, resMatches] = await Promise.all([
+      client.query(queryPrimerosSegundos),
+      client.query(queryTerceros),
+      client.query(queryMatches)
+    ]);
+
+    const primerosSegundos = resPrimerosSegundos.rows;
+    const terceros = resTerceros.rows;
+    const matches = resMatches.rows;
+
+    // Crear mapas para fácil acceso
+    const mapEquipos = {};
+    primerosSegundos.forEach(eq => {
+      mapEquipos[`${eq.grupo}${eq.posicion}`] = eq;
+    });
+    terceros.forEach((eq, idx) => {
+      mapEquipos[`T${idx + 1}`] = eq;
+    });
+
+    // Emparejamientos del Round of 32 (según FIFA World Cup 2026)
+    const emparejamientos = [
+      ['E', 1, 'T', 1],      // 1° E vs Mejor T
+      ['I', 1, 'T', 2],      // 1° I vs Mejor T
+      ['A', 2, 'B', 2],      // 2° A vs 2° B
+      ['F', 1, 'C', 2],      // 1° F vs 2° C
+      ['K', 2, 'L', 2],      // 2° K vs 2° L
+      ['H', 1, 'J', 2],      // 1° H vs 2° J
+      ['D', 1, 'T', 3],      // 1° D vs Mejor T
+      ['G', 1, 'T', 4],      // 1° G vs Mejor T
+      ['C', 1, 'F', 2],      // 1° C vs 2° F
+      ['E', 2, 'I', 2],      // 2° E vs 2° I
+      ['A', 1, 'T', 5],      // 1° A vs Mejor T
+      ['L', 1, 'T', 6],      // 1° L vs Mejor T
+      ['J', 1, 'H', 2],      // 1° J vs 2° H
+      ['D', 2, 'G', 2],      // 2° D vs 2° G
+      ['B', 1, 'T', 7],      // 1° B vs Mejor T
+      ['K', 1, 'T', 8]       // 1° K vs Mejor T
+    ];
+
+    // Función para construir match
+    const buildMatch = (matchId, equipo1Key, equipo2Key) => {
+      const eq1 = mapEquipos[equipo1Key];
+      const eq2 = mapEquipos[equipo2Key];
+      const existingMatch = matches.find(m => m.id === matchId);
 
       return {
         id: matchId,
-        equipo_1_id: eq1?.id,
-        equipo_1: eq1?.equipo,
-        codigo_1: eq1?.codigo,
-        bandera_1: BANDERAS[eq1?.codigo] || null,
-        equipo_2_id: eq2?.id,
-        equipo_2: eq2?.equipo,
-        codigo_2: eq2?.codigo,
-        bandera_2: BANDERAS[eq2?.codigo] || null
+        equipo_1_id: eq1?.id || null,
+        equipo_1: eq1?.nombre || null,
+        codigo_1: eq1?.pais_codigo || null,
+        bandera_1: eq1 ? (BANDERAS[eq1.pais_codigo] || 'https://flagcdn.com/w40/un.png') : null,
+        equipo_2_id: eq2?.id || null,
+        equipo_2: eq2?.nombre || null,
+        codigo_2: eq2?.pais_codigo || null,
+        bandera_2: eq2 ? (BANDERAS[eq2.pais_codigo] || 'https://flagcdn.com/w40/un.png') : null,
+        goles_1: existingMatch?.goles_1 ?? null,
+        goles_2: existingMatch?.goles_2 ?? null,
+        ganador_id: existingMatch?.ganador_id ?? null
       };
     };
 
-    const emparejamientos = [
-      ['E', 1, 'T', 1], ['I', 1, 'T', 2], ['A', 2, 'B', 2], ['F', 1, 'C', 2],
-      ['K', 2, 'L', 2], ['H', 1, 'J', 2], ['D', 1, 'T', 3], ['G', 1, 'T', 4],
-      ['C', 1, 'F', 2], ['E', 2, 'I', 2], ['A', 1, 'T', 5], ['L', 1, 'T', 6],
-      ['J', 1, 'H', 2], ['D', 2, 'G', 2], ['B', 1, 'T', 7], ['K', 1, 'T', 8]
-    ];
-
+    // Construir Round of 32
     const roundOf32 = emparejamientos.map((emp, idx) => {
       const eq1Key = `${emp[0]}${emp[1]}`;
+      const eq2Key = emp[2] === 'T' ? `T${emp[3]}` : `${emp[2]}${emp[3]}`;
+      
       const realMatchId = 17 + idx;
       const existingMatch = matches.find(m => m.id === realMatchId);
-
-      let baseMatch = buildMatch(realMatchId, eq1Key, null);
-
-      // Si es un partido con tercero, asignar el tercero correspondiente
-      if (partidosConTerceros.hasOwnProperty(realMatchId)) {
-        const terceroIdx = partidosConTerceros[realMatchId];
-        if (terceros[terceroIdx]) {
-          const terceroEquipo = equipos.find(e => e.id === terceros[terceroIdx].id);
-          baseMatch.equipo_2_id = terceroEquipo?.id;
-          baseMatch.equipo_2 = terceroEquipo?.equipo;
-          baseMatch.codigo_2 = terceroEquipo?.codigo;
-          baseMatch.bandera_2 = BANDERAS[terceroEquipo?.codigo] || null;
-        }
-      } else {
-        // Partidos normales sin tercero
-        const eq2Key = emp[2] === 'T' ? `T${emp[3]}` : `${emp[2]}${emp[3]}`;
-        const eq2 = equipos.find(e => e.codigo === eq2Key);
-        baseMatch.equipo_2_id = eq2?.id;
-        baseMatch.equipo_2 = eq2?.equipo;
-        baseMatch.codigo_2 = eq2?.codigo;
-        baseMatch.bandera_2 = BANDERAS[eq2?.codigo] || null;
-      }
-
+      
+      const baseMatch = buildMatch(realMatchId, eq1Key, eq2Key);
+      
       // Calcular ganador basado en goles
       let ganador_id = null;
-      if (existingMatch?.goles_1 !== null && existingMatch?.goles_1 !== undefined &&
+      if (existingMatch?.goles_1 !== null && existingMatch?.goles_1 !== undefined && 
           existingMatch?.goles_2 !== null && existingMatch?.goles_2 !== undefined) {
         if (existingMatch.goles_1 > existingMatch.goles_2) {
           ganador_id = baseMatch.equipo_1_id;
@@ -131,7 +194,7 @@ exports.handler = async (event) => {
           ganador_id = baseMatch.equipo_2_id;
         }
       }
-
+      
       return {
         ...baseMatch,
         goles_1: existingMatch?.goles_1 || null,
@@ -141,41 +204,89 @@ exports.handler = async (event) => {
       };
     });
 
-    // Construir RO16 desde ganadores de RO32
+    // TODO: Construir Round of 16, QF, SF, Final basados en ganadores de Round of 32
     const roundOf16 = [];
-    for (let i = 0; i < 8; i++) {
-      const match1 = roundOf32[i * 2];
-      const match2 = roundOf32[i * 2 + 1];
-
-      const eq1 = match1.ganador_id ? 
-        equipos.find(e => e.id === match1.ganador_id) : null;
-      const eq2 = match2.ganador_id ? 
-        equipos.find(e => e.id === match2.ganador_id) : null;
-
+    
+    // Construir Round of 16 DINÁMICAMENTE desde ganadores del RO32
+    // Agrupar RO32 en pares: (1,2), (3,4), (5,6), (7,8), (9,10), (11,12), (13,14), (15,16)
+    for (let i = 0; i < roundOf32.length; i += 2) {
+      const match1 = roundOf32[i];
+      const match2 = roundOf32[i + 1];
+      
+      // ID del RO16: 33-40
+      const ro16Id = 33 + (i / 2);
+      const existingMatch = matches.find(m => m.id === ro16Id);
+      
+      // Obtener ganadores (si no hay, será null = "A definir")
+      const ganador1Id = match1?.ganador_id || null;
+      const ganador2Id = match2?.ganador_id || null;
+      
+      // Buscar datos del ganador 1
+      let eq1Id = null, eq1Nombre = null, eq1Codigo = null, eq1Bandera = null;
+      if (ganador1Id) {
+        if (ganador1Id === match1?.equipo_1_id) {
+          eq1Id = match1?.equipo_1_id;
+          eq1Nombre = match1?.equipo_1;
+          eq1Codigo = match1?.codigo_1;
+          eq1Bandera = match1?.bandera_1;
+        } else {
+          eq1Id = match1?.equipo_2_id;
+          eq1Nombre = match1?.equipo_2;
+          eq1Codigo = match1?.codigo_2;
+          eq1Bandera = match1?.bandera_2;
+        }
+      }
+      
+      // Buscar datos del ganador 2
+      let eq2Id = null, eq2Nombre = null, eq2Codigo = null, eq2Bandera = null;
+      if (ganador2Id) {
+        if (ganador2Id === match2?.equipo_1_id) {
+          eq2Id = match2?.equipo_1_id;
+          eq2Nombre = match2?.equipo_1;
+          eq2Codigo = match2?.codigo_1;
+          eq2Bandera = match2?.bandera_1;
+        } else {
+          eq2Id = match2?.equipo_2_id;
+          eq2Nombre = match2?.equipo_2;
+          eq2Codigo = match2?.codigo_2;
+          eq2Bandera = match2?.bandera_2;
+        }
+      }
+      
       roundOf16.push({
-        id: 33 + i,
-        equipo_1: eq1?.equipo || 'A definir',
-        equipo_1_id: eq1?.id || null,
-        codigo_1: eq1?.codigo,
-        bandera_1: eq1 ? BANDERAS[eq1.codigo] : null,
-        equipo_2: eq2?.equipo || 'A definir',
-        equipo_2_id: eq2?.id || null,
-        codigo_2: eq2?.codigo,
-        bandera_2: eq2 ? BANDERAS[eq2.codigo] : null,
-        ganador_id: null,
-        goles_1: null,
-        goles_2: null,
-        fecha_hora: null
+        id: ro16Id,
+        fase: 'RO16',
+        partido_numero: (i / 2) + 1,
+        equipo_1_id: eq1Id,
+        equipo_1: eq1Nombre,
+        codigo_1: eq1Codigo,
+        bandera_1: eq1Bandera,
+        equipo_2_id: eq2Id,
+        equipo_2: eq2Nombre,
+        codigo_2: eq2Codigo,
+        bandera_2: eq2Bandera,
+        goles_1: existingMatch?.goles_1 || null,
+        goles_2: existingMatch?.goles_2 || null,
+        ganador_id: existingMatch?.ganador_id || null,
+        fecha_hora: existingMatch?.fecha_hora || null
       });
     }
 
-    await client.end();
+    const quarterfinals = [];
+    const semifinals = [];
+    const final = null;
+    const thirdPlace = null;
 
     return {
       statusCode: 200,
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         roundOf32,
-        roundOf16
+        roundOf16,
+        quarterfinals,
+        semifinals,
+        final,
+        thirdPlace
       })
     };
   } catch (error) {
@@ -184,5 +295,7 @@ exports.handler = async (event) => {
       statusCode: 500,
       body: JSON.stringify({ error: error.message })
     };
+  } finally {
+    await client.end();
   }
 };
--- netlify/functions/obtener-terceros.js
+++ netlify/functions/obtener-terceros.js
@@ -1,25 +1,117 @@
 const { Client } = require('pg');
 
-exports.handler = async (event) => {
-  try {
-    const client = new Client({
-      connectionString: process.env.DATABASE_URL
-    });
+// Mapeo de códigos de país a URLs de banderas
+const BANDERAS = {
+  'MEX': 'https://flagcdn.com/w40/mx.png',
+  'SUD': 'https://flagcdn.com/w40/za.png',
+  'COR': 'https://flagcdn.com/w40/kr.png',
+  'CZE': 'https://flagcdn.com/w40/cz.png',
+  'CAN': 'https://flagcdn.com/w40/ca.png',
+  'BIH': 'https://flagcdn.com/w40/ba.png',
+  'QAT': 'https://flagcdn.com/w40/qa.png',
+  'SUI': 'https://flagcdn.com/w40/ch.png',
+  'BRA': 'https://flagcdn.com/w40/br.png',
+  'MAR': 'https://flagcdn.com/w40/ma.png',
+  'HAI': 'https://flagcdn.com/w40/ht.png',
+  'ESC': 'https://flagcdn.com/w40/gb-sct.png',
+  'USA': 'https://flagcdn.com/w40/us.png',
+  'PAR': 'https://flagcdn.com/w40/py.png',
+  'AUS': 'https://flagcdn.com/w40/au.png',
+  'TUR': 'https://flagcdn.com/w40/tr.png',
+  'ALE': 'https://flagcdn.com/w40/de.png',
+  'CUW': 'https://flagcdn.com/w40/cw.png',
+  'CIV': 'https://flagcdn.com/w40/ci.png',
+  'ECU': 'https://flagcdn.com/w40/ec.png',
+  'HOL': 'https://flagcdn.com/w40/nl.png',
+  'JAP': 'https://flagcdn.com/w40/jp.png',
+  'SUE': 'https://flagcdn.com/w40/se.png',
+  'TUN': 'https://flagcdn.com/w40/tn.png',
+  'BEL': 'https://flagcdn.com/w40/be.png',
+  'EGY': 'https://flagcdn.com/w40/eg.png',
+  'IRN': 'https://flagcdn.com/w40/ir.png',
+  'NZL': 'https://flagcdn.com/w40/nz.png',
+  'ESP': 'https://flagcdn.com/w40/es.png',
+  'CPV': 'https://flagcdn.com/w40/cv.png',
+  'ARS': 'https://flagcdn.com/w40/sa.png',
+  'URU': 'https://flagcdn.com/w40/uy.png',
+  'FRA': 'https://flagcdn.com/w40/fr.png',
+  'SEN': 'https://flagcdn.com/w40/sn.png',
+  'IRQ': 'https://flagcdn.com/w40/iq.png',
+  'NOR': 'https://flagcdn.com/w40/no.png',
+  'ARG': 'https://flagcdn.com/w40/ar.png',
+  'ALG': 'https://flagcdn.com/w40/dz.png',
+  'AUT': 'https://flagcdn.com/w40/at.png',
+  'JOR': 'https://flagcdn.com/w40/jo.png',
+  'POR': 'https://flagcdn.com/w40/pt.png',
+  'COG': 'https://flagcdn.com/w40/cg.png',
+  'UZB': 'https://flagcdn.com/w40/uz.png',
+  'COL': 'https://flagcdn.com/w40/co.png',
+  'ING': 'https://flagcdn.com/w40/gb-eng.png',
+  'CRO': 'https://flagcdn.com/w40/hr.png',
+  'GHA': 'https://flagcdn.com/w40/gh.png',
+  'PAN': 'https://flagcdn.com/w40/pa.png'
+};
 
+exports.handler = async (event) => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL
+  });
+
+  try {
     await client.connect();
 
+    // Obtener el 3er clasificado de cada grupo usando ROW_NUMBER
     const query = `
-      SELECT id, equipo
-      FROM tabla_posiciones
-      ORDER BY id
+      WITH ranked AS (
+        SELECT 
+          e.id,
+          e.nombre,
+          e.pais_codigo,
+          e.grupo,
+          tp.puntos,
+          tp.partidos_jugados,
+          tp.goles_a_favor,
+          tp.goles_en_contra,
+          (tp.goles_a_favor - tp.goles_en_contra) as diferencia_goles,
+          ROW_NUMBER() OVER (
+            PARTITION BY tp.grupo 
+            ORDER BY tp.puntos DESC, (tp.goles_a_favor - tp.goles_en_contra) DESC
+          ) as rank
+        FROM tabla_posiciones tp
+        JOIN equipos e ON tp.equipo_id = e.id
+      )
+      SELECT 
+        id,
+        nombre,
+        pais_codigo,
+        grupo,
+        puntos,
+        partidos_jugados,
+        goles_a_favor,
+        goles_en_contra,
+        diferencia_goles
+      FROM ranked
+      WHERE rank = 3
+      ORDER BY puntos DESC, diferencia_goles DESC
     `;
 
     const result = await client.query(query);
-    await client.end();
+
+    console.log(`Encontrados ${result.rows.length} terceros clasificados`);
+
+    const terceros = result.rows.map((row, idx) => ({
+      posicion: idx + 1,
+      ...row,
+      bandera: BANDERAS[row.pais_codigo] || 'https://flagcdn.com/w40/un.png'
+    }));
 
     return {
       statusCode: 200,
-      body: JSON.stringify(result.rows)
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        terceros: terceros,
+        total: terceros.length
+      })
     };
   } catch (error) {
     console.error('Error:', error);
@@ -27,5 +119,7 @@ exports.handler = async (event) => {
       statusCode: 500,
       body: JSON.stringify({ error: error.message })
     };
+  } finally {
+    await client.end();
   }
 };
